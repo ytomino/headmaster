dbf0c3a0e089b6e3d7eeaed425ca8b402f80ecdd
yt <demoonlit@panathenaia.halfmoon.jp>
2012-12-30 12:34:58 +0900

    Improve define-parser

--- source/c_define_parser.ml
+++ source/c_define_parser.ml
@@ -45,7 +45,6 @@ struct
 			| `any of string];;
 		
 		val parse_define:
-			?name: string ->
 			(ranged_position -> string -> unit) ->
 			(ranged_position -> string -> [> known_errors_of_define_parser] -> bool) ->
 			language ->
@@ -74,7 +73,34 @@ module DefineParser
 	: DefineParserType (Literals) (LexicalElement) (Preprocessor) (Syntax) (Parser).S =
 struct
 	
+	(* error messages *)
+	
+	let failed_to_preprocess (s: string): string =
+		"macro " ^ s ^ " is failed to preprocess.";;
+	let failed_to_parse (s: string): string =
+		"macro " ^ s ^ " is failed to parse.";;
+	let function_macro_failed_to_parse (s: string): string =
+		"function macro " ^ s ^ " is failed to parse.";;
+	let assert_failed (s: string): string =
+		"assert is failed to parsing macro " ^ s ^ ". (define-parser)";;
+	
+	(* additional parser *)
+	
 	type 'a p = 'a Syntax.p;;
+	
+	let parse_operator_option
+		(xs: 'a Parser.in_t)
+		: operator p option * 'a Parser.in_t =
+	(
+		begin match xs with
+		| lazy (`cons (op_p, (#operator as op_e), xs)) ->
+			Some (op_p, op_e), xs
+		| _ ->
+			None, xs
+		end
+	);;
+	
+	(* macro handling *)
 	
 	let rec has_sharps (xs: 'a Parser.in_t): bool = (
 		begin match xs with
@@ -89,26 +115,6 @@ struct
 			false
 		end
 	);;
-	
-	let parse_operator_option
-		(xs: 'a Parser.in_t)
-		: operator p option * 'a Parser.in_t =
-	(
-		begin match xs with
-		| lazy (`cons (op_p, (#operator as op_e), xs)) ->
-			Some (op_p, op_e), xs
-		| _ ->
-			None, xs
-		end
-	);;
-	
-	type define = [
-		| `operator of operator
-		| `declaration_specifiers of Syntax.declaration_specifiers
-		| `initializer_t of Syntax.initializer_t
-		| `function_expr of (string p * [`typedef | `value]) list * [`varargs | `none] * Syntax.expression
-		| `function_stmt of (string p * [`typedef | `value]) list * [`varargs | `none] * Syntax.statement
-		| `any of string];;
 	
 	let is_alias_of_other_macro
 		(macros: Preprocessor.define_map)
@@ -130,8 +136,17 @@ struct
 		end
 	);;
 	
-	let rec parse_define
-		?(name: string option)
+	(* define parser *)
+	
+	type define = [
+		| `operator of operator
+		| `declaration_specifiers of Syntax.declaration_specifiers
+		| `initializer_t of Syntax.initializer_t
+		| `function_expr of (string p * [`typedef | `value]) list * [`varargs | `none] * Syntax.expression
+		| `function_stmt of (string p * [`typedef | `value]) list * [`varargs | `none] * Syntax.statement
+		| `any of string];;
+	
+	let parse_define
 		(error: ranged_position -> string -> unit)
 		(is_known_error: ranged_position -> string -> [> known_errors_of_define_parser] -> bool)
 		(lang: language)
@@ -140,143 +155,128 @@ struct
 		(macro: Preprocessor.define_item)
 		: define p =
 	(
-		let ps = macro.Preprocessor.df_position in
-		if is_known_error ps macro.Preprocessor.df_name `unparsible_macro then ps, (`any "unparsible") else
-		let name =
-			begin match name with
-			| Some name ->
-				name
-			| None ->
-				macro.Preprocessor.df_name
+		let rec process
+			(macros: Preprocessor.define_map)
+			(macro_name: string)
+			(macro: Preprocessor.define_item)
+			: define p =
+		(
+			let ps = macro.Preprocessor.df_position in
+			if is_known_error ps macro.Preprocessor.df_name `unparsible_macro then ps, (`any "unparsible") else
+			let other_macros = StringMap.remove macro.Preprocessor.df_name macros in
+			begin match is_alias_of_other_macro macros macro macro.Preprocessor.df_contents with
+			| Some target when target != macro ->
+				(* deriving *)
+				let _, result = process other_macros macro_name target in
+				ps, result
+			| _ ->
+				let xs = macro.Preprocessor.df_contents in
+				let has_arguments = macro.Preprocessor.df_has_arguments in
+				if has_arguments && has_sharps xs then ps, (`any "has # or ##") else (* exclude macros having # or ## *)
+				let has_error = ref false in
+				let dummy_error _ _ = has_error := true in
+				let xs = lazy (Preprocessor.preprocess
+					dummy_error
+					is_known_error
+					lang
+					(fun ~current ?next _ _ _ ->
+						ignore current;
+						ignore next;
+						has_error := true;
+						(LazyList.find_nil xs :> Preprocessor.in_prim))
+					`top_level
+					other_macros
+					StringMap.empty
+					xs)
+				in
+				let (_: [`nil of ranged_position * Preprocessor.define_map]) = LazyList.find_nil xs in (* error check *)
+				begin try
+					if !has_error then (
+						error ps (failed_to_preprocess macro_name);
+						ps, (`any "preprocessor error")
+					) else if LazyList.is_empty xs then (
+						ps, (`any "empty")
+					) else if has_arguments then (
+						let varargs = if macro.Preprocessor.df_varargs then `varargs else `none in
+						let last_f result =
+							error ps (function_macro_failed_to_parse macro_name);
+							result
+						in
+						list_combination (fun cont args dummy_result ->
+							let args =
+								List.map (fun (v, is_type) ->
+									v, (if is_type then `typedef else `value)
+								) args
+							in
+							let typedefs =
+								List.fold_right (fun ((_, k), is_type) v ->
+									if is_type = `typedef then TypedefSet.add k v else v
+								) args typedefs
+							in
+							let expr, xr = has_error := false; Parser.parse_expression_or_error dummy_error lang typedefs xs in
+							if not !has_error && LazyList.is_empty xr && expr <> `error then (
+								begin match expr with
+								| `some expr -> ps, `function_expr (args, varargs, snd expr)
+								| `error -> assert false
+								end
+							) else
+							let stmt, xr = has_error := false; Parser.parse_statement_or_error ~semicolon_need:false dummy_error lang typedefs xs in
+							if not !has_error && LazyList.is_empty xr && stmt <> `error then (
+								begin match stmt with
+								| `some stmt -> ps, `function_stmt (args, varargs, snd stmt)
+								| `error -> assert false
+								end
+							) else (
+								cont dummy_result
+							)
+						) macro.Preprocessor.df_args last_f (ps, `any "parser error")
+					) else (
+						begin match is_alias_of_other_macro macros macro xs with
+						| Some target when target != macro ->
+							(* deriving *)
+							let _, result = process other_macros macro_name target in
+							ps, result
+						| Some _ ->
+							(* self *)
+							ps, `any "repeating itself"
+						| None ->
+							let op, xr = has_error := false; parse_operator_option xs in
+							if not !has_error && LazyList.is_empty xr && op <> None then (
+								begin match op with
+								| Some op -> ps, `operator (snd op)
+								| None -> assert false
+								end
+							) else
+							let spec, xr = has_error := false; Parser.parse_declaration_specifiers_option dummy_error lang typedefs xs in
+							if not !has_error && LazyList.is_empty xr && spec <> `none then (
+								begin match spec with
+								| `some spec -> ps, `declaration_specifiers (snd spec)
+								| `none -> assert false
+								end
+							) else
+							let expr, xr = has_error := false; Parser.parse_initializer_or_error dummy_error lang typedefs xs in
+							if not !has_error && LazyList.is_empty xr && expr <> `error then (
+								begin match expr with
+								| `some expr -> ps, `initializer_t (snd expr)
+								| `error -> assert false
+								end
+							) else (
+								error ps (failed_to_parse macro_name);
+								ps, `any "parser error"
+							)
+						end
+					)
+				with Assert_failure _ as e ->
+					prerr_string (Printexc.to_string e);
+					prerr_newline ();
+					Printexc.print_backtrace stderr;
+					flush stderr;
+					error ps (assert_failed macro_name);
+					raise e
+				end
 			end
-		in
-		begin match is_alias_of_other_macro macros macro macro.Preprocessor.df_contents with
-		| Some target when target != macro ->
-			(* deriving *)
-			let _, result = parse_define
-				~name
-				error
-				is_known_error
-				lang
-				typedefs
-				(StringMap.remove macro.Preprocessor.df_name macros)
-				target
-			in
-			ps, result
-		| _ ->
-			let xs = macro.Preprocessor.df_contents in
-			let has_arguments = macro.Preprocessor.df_has_arguments in
-			if has_arguments && has_sharps xs then ps, (`any "has # or ##") else (* exclude macros having # or ## *)
-			let has_error = ref false in
-			let dummy_error _ _ = has_error := true in
-			let xs = lazy (Preprocessor.preprocess
-				dummy_error
-				is_known_error
-				lang
-				(fun ~current ?next _ _ _ ->
-					ignore current;
-					ignore next;
-					has_error := true;
-					(LazyList.find_nil xs :> Preprocessor.in_prim))
-				`top_level
-				(StringMap.remove macro.Preprocessor.df_name macros)
-				StringMap.empty
-				xs)
-			in
-			let (_: [`nil of ranged_position * Preprocessor.define_map]) = LazyList.find_nil xs in (* error check *)
-			begin try
-				if !has_error then (
-					error ps ("macro " ^ name ^ " could not be parsed. (preprocessor error)");
-					ps, (`any "preprocessor error")
-				) else if LazyList.is_empty xs then (
-					ps, (`any "empty")
-				) else if has_arguments then (
-					let varargs = if macro.Preprocessor.df_varargs then `varargs else `none in
-					let last_f result =
-						error ps ("function-macro " ^ name ^ " could not be parsed.");
-						result
-					in
-					list_combination (fun cont args dummy_result ->
-						let args =
-							List.map (fun (v, is_type) ->
-								v, (if is_type then `typedef else `value)
-							) args
-						in
-						let typedefs =
-							List.fold_right (fun ((_, k), is_type) v ->
-								if is_type = `typedef then TypedefSet.add k v else v
-							) args typedefs
-						in
-						let expr, xr = has_error := false; Parser.parse_expression_or_error dummy_error lang typedefs xs in
-						if not !has_error && LazyList.is_empty xr && expr <> `error then (
-							begin match expr with
-							| `some expr -> ps, `function_expr (args, varargs, snd expr)
-							| `error -> assert false
-							end
-						) else
-						let stmt, xr = has_error := false; Parser.parse_statement_or_error ~semicolon_need:false dummy_error lang typedefs xs in
-						if not !has_error && LazyList.is_empty xr && stmt <> `error then (
-							begin match stmt with
-							| `some stmt -> ps, `function_stmt (args, varargs, snd stmt)
-							| `error -> assert false
-							end
-						) else (
-							cont dummy_result
-						)
-					) macro.Preprocessor.df_args last_f (ps, `any "parser error")
-				) else (
-					begin match is_alias_of_other_macro macros macro xs with
-					| Some target when target != macro ->
-						(* deriving *)
-						let _, result = parse_define
-							~name
-							error
-							is_known_error
-							lang
-							typedefs
-							(StringMap.remove macro.Preprocessor.df_name macros)
-							target
-						in
-						ps, result
-					| Some _ ->
-						(* self *)
-						ps, `any "repeating itself"
-					| None ->
-						let op, xr = has_error := false; parse_operator_option xs in
-						if not !has_error && LazyList.is_empty xr && op <> None then (
-							begin match op with
-							| Some op -> ps, `operator (snd op)
-							| None -> assert false
-							end
-						) else
-						let spec, xr = has_error := false; Parser.parse_declaration_specifiers_option dummy_error lang typedefs xs in
-						if not !has_error && LazyList.is_empty xr && spec <> `none then (
-							begin match spec with
-							| `some spec -> ps, `declaration_specifiers (snd spec)
-							| `none -> assert false
-							end
-						) else
-						let expr, xr = has_error := false; Parser.parse_initializer_or_error dummy_error lang typedefs xs in
-						if not !has_error && LazyList.is_empty xr && expr <> `error then (
-							begin match expr with
-							| `some expr -> ps, `initializer_t (snd expr)
-							| `error -> assert false
-							end
-						) else (
-							error ps ("macro " ^ name ^ " could not be parsed.");
-							ps, `any "parser error"
-						)
-					end
-				)
-			with Assert_failure _ as e ->
-				prerr_string (Printexc.to_string e);
-				prerr_newline ();
-				Printexc.print_backtrace stderr;
-				flush stderr;
-				error ps ("macro " ^ name ^ " could not be parsed. (assert failure)");
-				raise e
-			end
-		end
+		) in
+		process macros macro.Preprocessor.df_name macro
 	);;
 	
 	let map
