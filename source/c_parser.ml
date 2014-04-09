4debffc4a316e81c02cbffd07ee185ece7532e2e
yt <demoonlit@panathenaia.halfmoon.jp>
2014-04-09 15:36:10 +0900

    Parse double-underline form of __attribute__((__visibility__(...)))

--- source/c_parser.ml
+++ source/c_parser.ml
@@ -821,7 +821,7 @@ struct
 				let r_paren, xs = parse_r_paren_or_error error xs in
 				let `some (ps, ()) = (`some n) &^l_paren &^ arg &^ r_paren in
 				`some (ps, `vector_size (n, l_paren, arg, r_paren)), xs
-			| "visibility" ->
+			| "visibility" | "__visibility__" ->
 				let n = p4, attr_keyword in
 				let l_paren, xs = parse_l_paren_or_error error xs in
 				let arg, xs = parse_chars_literal_or_error error lang typedefs xs in
