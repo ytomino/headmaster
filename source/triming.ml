faf31553ddd406cfc8a783b70243c337cbf966d5
ytomino <demoonlit@panathenaia.halfmoon.jp>
2020-10-25 01:12:32 +0900

    Change the meaning of the last parameters of trim_left and trim_right
    From "next" to "length".

--- source/triming.ml
+++ source/triming.ml
@@ -1,20 +1,20 @@
 let rec trim_left
 	(sub: string -> int -> int -> 'a)
 	(f: char -> bool)
-	(s: string) (start: int) (next: int): 'a =
+	(s: string) (start: int) (len: int): 'a =
 (
-	if start >= next || not (f s.[start]) then sub s start (next - start) else
-	trim_left sub f s (start + 1) next
+	if len <= 0 || not (f s.[start]) then sub s start len else
+	trim_left sub f s (start + 1) (len - 1)
 );;
 
 let rec trim_right
 	(sub: string -> int -> int -> 'a)
 	(f: char -> bool)
-	(s: string) (start: int) (next: int): 'a =
+	(s: string) (start: int) (len: int): 'a =
 (
-	let p_next = next - 1 in
-	if start >= next || not (f s.[p_next]) then sub s start (next - start) else
-	trim_right sub f s start p_next
+	let pred_len = len - 1 in
+	if len <= 0 || not (f s.[start + pred_len]) then sub s start len else
+	trim_right sub f s start pred_len
 );;
 
 let trim
@@ -31,7 +31,7 @@ let take_word
 	let is_not_separator c = not (is_separator c) in
 	trim_left (fun s start len ->
 		let hd = String.sub s 0 start in
-		let tl = trim_left String.sub is_separator s start (start + len) in
+		let tl = trim_left String.sub is_separator s start len in
 		hd, tl
 	) is_not_separator s 0 (String.length s)
 );;
